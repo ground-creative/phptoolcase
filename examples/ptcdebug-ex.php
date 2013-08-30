a2786c0b2b6dc684bb240200cd53a90c346c7d18
Carlo Pietrobattista <carlo@salapc.com>
2013-08-30 18:30:46 +0200

    debugger & logger examples

--- examples/ptcdebug-ex.php
+++ examples/ptcdebug-ex.php
@@ -1,45 +1,67 @@
-<?
-	//session_start();				# start session for persistent debugging
-	
-	$_GET['debug']=true;       		# turn on the debug
-	
-	//$_GET['debugOff']=true;    		# turn off debug
-	
-	require_once('../PtcDebug.php');	# include the PtcDebug class
-	
-	$options=array
+<? 
+	session_start();				// start session for persistent debugging and code highlighter popup
+
+	$_GET['debug']=true;       		// turn on the debug
+
+	//$_GET['debugOff']=true;    		// turn off debug
+
+	require_once('../PtcDebug.php');	// include the PtcDebug class
+
+	$options=array				// add some options before class initialization
 	(
 		'url_key'		=>	'debug',
 		'url_pass'		=>	'true',
 		'die_on_error'	=>	false,
 	);
-	
-	PtcDebug::setErrorHandler($options['die_on_error']);# set error handler to be the debug class
-	
-	PtcDebug::debugLoader($options);
 
-	PtcDebug::bufferLog('debug self initialized ','this is the result msg');
+	PtcDebug::load($options);		// initialize the class
 
-	trigger_error("the rorrss",E_USER_NOTICE);
+	/* LOGGING A MESSAGE */
+	log_msg('just a message');
 	
-	trigger_error("the rorrss",E_USER_WARNING);
+	/* LOGGING A VARIABLE WITH A STATEMENT */
+	$var='something';
+	log_msg($var,'testing a variable');
 	
-	trigger_error("the rorrss333333333333333",E_USER_ERROR);
+	/* LOGGING AN ARRAY TO THE MESSAGE PANEL WITH A DIFFERENT CATEGORY */
+	$array=array('key'=>'value','key1'=>'value1');
+	log_msg($array,'testing an array','new category');
+
+	/* THROWING A NOTICE */
+	trigger_error('some notice',E_USER_NOTICE);
+
+	/* THROWING A WARNING */
+	trigger_error('some warning',E_USER_WARNING);
+
+	/* THROWING AN ERROR */
+	trigger_error('some error',E_USER_ERROR);	// continue execution with the options "die_on_error" set to false
+
+	/* TESTING AN ERROR WITHIN A FUNCTION */
+	function some_func(){ fopen(); }
+	echo some_func();						// will throw an error
 	
-	PtcDebug::bufferLog(32,'some statement');	
-	PtcDebug::stopTimer();
-	
-	PtcDebug::bufferSql("select from where something",'some other statement');
-	PtcDebug::stopTimer();
-	
-	PtcDebug::bufferLog(array("dasd"=>"dasdsa","daasd"=>"23333333"),'some other statement');
-	
-	function ddas()
+	/* LOGGING SQL QUERIES AND TIMING EXECUTION */
+	$sql='select from where something';	// some sql query, will be used as reference
+	log_sql('',$sql);			// leaving the first parameter empty, can be added later with the query result
+	$sql_result=array('key'=>'value','key1'=>'value1');	// this should be the sql result of the sql query
+	stop_timer($sql);					// time execution, the query is used as reference
+	add_to_log($sql,$sql_result);			// attaching the result to the message based on the reference
+
+	/* WATCHING A VARIABLE */
+	declare(ticks=1)
 	{
-		fopen();
+		$var='some test';
+		watch_var('var');					// passing the variable without the "$" symbol
+		$var='some new value';				// the variable changed
 	}
 	
-	echo ddas();
-
+	/* TIMING A LOOP */
+	log_msg('','timing a loop');	// leaving the first parameter empty
+	for($i=0;$i<100;$i++){ @$a[]=$i; }
+	stop_timer('timing a loop');	// using the reference to attach the execution time to the buffer
+	
+	/* CATCHING AN EXCEPTION */
+	throw new Exception('Uncaught Exception');
+	
 	//session_destroy();
 ?>
