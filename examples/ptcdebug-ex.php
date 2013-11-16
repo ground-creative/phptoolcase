75e0f15b7f3de8a38f020ce17af09664a183813c
Carlo Pietrobattista <carlo@salapc.com>
2013-11-16 19:34:20 +0100

    debugger & logger example file

--- examples/ptcdebug-ex.php
+++ examples/ptcdebug-ex.php
@@ -4,19 +4,22 @@
 	* EXAMPLE FILE FOR PTCDEBUG CLASS
 	*/
 
-	session_start( );		// start session for persistent debugging and code highlighter popup
+	session_start( );				// start session for persistent debugging and code highlighter popup
+
+	declare(ticks=1);				// declare globally for the code coverage and function calls trace
 
 	$_GET[ 'debug' ] = true;       		// turn on the debug
 
-	//$_GET['debug_off']=true;    		// turn off debug
+	//$_GET[ 'debug_off' ] = true;    	// turn off debug
 
 	require_once( '../PtcDebug.php' );	// include the PtcDebug class
 
-	$options=array				// add some options before class initialization
+	$options = array				// add some options before class initialization
 	(
-		'url_key'		=>	'debug',
-		'url_pass'		=>	'true',
-		'die_on_error'	=>	false,	// continue if fatal error
+		'url_key'			=>	'debug' ,
+		'url_pass'			=>	'true' ,
+		'die_on_error'		=>	false ,// continue if fatal error
+		'debug_console'	=>	true , // send messages to console, chrome only with php-console extension
 	);
 
 	PtcDebug::load( $options );		// initialize the class
@@ -43,6 +46,7 @@
 	$array = array( 'key' => 'value' , 'key1' => 'value1' );
 	PtcDebug::bufferLog( $array , 'testing an array' , 'new category' );
 	
+	
 	/* LOGGING AN OBJECT */
 	PtcDebug::bufferLog( ( object ) $array , 'testing an object' );
 
@@ -60,7 +64,7 @@
 
 
 	/* TESTING AN ERROR WITHIN A FUNCTION */
-	function some_func( ) { fopen( ); }
+	function some_func( ){ fopen( ); }
 	echo some_func( );						// will throw an error
 
 
@@ -73,17 +77,18 @@
 
 
 	/* WATCHING A VARIABLE */	
-	declare(ticks=1)
+	declare(ticks=1)						// declaring code block it is more precise for watching vars
 	{
 		$var = 'some test';
 		PtcDebug::watch( 'var' );				// passing the variable without the "$" symbol
 		$var = 'some new value';				// the variable changed
 	}
 	
+	
 	/* TIMING A LOOP */
 	PtcDebug::bufferLog( '' , 'timing a loop' );	// leaving the first parameter empty
-	for ( $i = 0; $i < 100; $i++ ) { @$a[ ] = $i; }
-	PtcDebug::stopTimer( 'timing a loop' );	// using the reference to attach the execution time to the buffer
+	for ( $i = 0; $i < 100; $i++ ){ @$a[ ] = $i; }
+	PtcDebug::stopTimer( 'timing a loop' );		// using the reference to attach the execution time to the buffer
 	
 	
 	/* STOP CODE COVERAGE ANALYSIS */
@@ -94,7 +99,15 @@
 	PtcDebug::stopTrace( );		// we could start it again later, if stopTrace( ) is not used it will be stopped at shutdown
 	
 	
+	/* DOWLOAD PHP-CONSOLE FOR CHROME TO SEE MESSAGES IN CONSOLE */
+	PtcDebug::bufferLog( '' , '<span style="color:red;">**For Chrome Browser:</span> 
+						<a target="_blank" href="https://chrome.google.com/webstore/detail/php-console/nfhmhhlpfleoednkpnnnkolmclajemef">
+							Download php-console</a> chrome extension to see debug output in console');
+	
+	
 	/* CATCHING AN EXCEPTION */
 	throw new Exception( 'Uncaught Exception' );
 	
+	
 	//session_destroy();
+
