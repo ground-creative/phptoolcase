e36eb45a955e15501ce394122afeb8f8c34ed0e4
Carlo Pietrobattista <carlo@salapc.com>
2013-11-16 19:31:58 +0100

    ptc helper functions for the components

--- ptc-helpers.php
+++ ptc-helpers.php
@@ -7,7 +7,7 @@
 	* PHP version 5.3
 	* @category 	Libraries
 	* @package  	PhpToolCase
-	* @version	0.8.4b
+	* @version	0.9.1b
 	* @author   	Irony <carlo@salapc.com>
 	* @license  	http://www.gnu.org/copyleft/gpl.html GNU General Public License
 	* @link     	http://phptoolcase.com
@@ -28,14 +28,6 @@
 		return ptc_run( 'bufferLog' , @_PTCDEBUG_NAMESPACE_ , array( $string , $statement , $category ) );
 	}
 	/**
-	* Writes data to the messages panel
-	* @deprecated	use {@link ptc_log()} instead
-	*/
-	function log_msg($string,$statement=null,$category=null)
-	{
-		return ptc_run( 'bufferLog' , @_PTCDEBUG_NAMESPACE_ , array( $string , $statement , $category ) );
-	}
-	/**
 	* Writes data to the sql panel
 	* @param 	mixed 	$string		the string to pass
 	* @param 	mixed 	$statement	some statement if required
@@ -48,14 +40,6 @@
 		return ptc_run( 'bufferSql' , @_PTCDEBUG_NAMESPACE_ , array( $string , $statement , $category ) );
 	}
 	/**
-	* Writes data to the sql panel
-	* @deprecated	use {@link ptc_log_sql()} instead
-	*/
-	function log_sql($string,$statement=null,$category=null)
-	{
-		return ptc_run( 'bufferSql' , @_PTCDEBUG_NAMESPACE_ , array( $string , $statement , $category ) );
-	}
-	/**
 	* Monitors the execution of php code, or sql queries based on a reference 
 	* @param	string			$reference	a reference to look for ("$statement")
 	* @param 	string|numeric 	$precision	sec/ms
@@ -63,14 +47,6 @@
 	* @tutorial	PtcDebug.cls#stopTimer
 	*/
 	function ptc_stop_timer( $reference = null , $precision = 1 )
-	{ 
-		return ptc_run( 'stopTimer' , @_PTCDEBUG_NAMESPACE_ , array( $reference , $precision ) );
-	}
-	/**
-	* Monitors the execution of php code, or sql queries based on a reference 
-	* @deprecated	use {@link ptc_stop_timer()} instead
-	*/
-	function stop_timer( $reference = null , $precision = 1 )
 	{ 
 		return ptc_run( 'stopTimer' , @_PTCDEBUG_NAMESPACE_ , array( $reference , $precision ) );
 	}
@@ -87,30 +63,14 @@
 		return ptc_run( 'addToBuffer' , @_PTCDEBUG_NAMESPACE_ , array( $reference , $string , $statement ) );
 	}
 	/**
-	* Attaches data to the buffer array based on a reference 
-	* @deprecated	use {@link ptc_attach()} instead
-	*/
-	function add_to_log( $reference , $string , $statement = null )
-	{
-		return ptc_run( 'addToBuffer' , @_PTCDEBUG_NAMESPACE_ , array( $reference , $string , $statement ) );
-	}
-	/**
 	* Watches a variable that is in a declare(ticks=n){ code block }, for changes 
 	* @param 	string 	$variableName		the name of the variable to watch
 	* @see PtcDebug::watch()
 	* @tutorial	PtcDebug.cls#watchVar
 	*/
-	function ptc_watch( $variableName ) 
+	function ptc_watch( $variableName , $callback = null ) 
 	{ 
-		return ptc_run( 'watch' , @_PTCDEBUG_NAMESPACE_ , array( $variableName ) );
-	}
-	/**
-	* Watches a variable that is in a declare(ticks=n){ code block }, for changes 
-	* @deprecated	use {@link ptc_watch()} instead
-	*/
-	function watch_var( $variableName ) 
-	{ 	
-		return ptc_run( 'watch' , @_PTCDEBUG_NAMESPACE_ , array( $variableName ) );
+		return ptc_run( 'watch' , @_PTCDEBUG_NAMESPACE_ , array( $variableName , $callback ) );
 	}
 	/*
 	* Starts the code coverage analysis utility to find executed lines
@@ -177,7 +137,7 @@
 		return ptc_run( 'addDirs' , @constant( $handyman ) , array( $directories ) );
 	}
 	/**
-	* Adds files to the autoloader to load classes
+	* Adds files to the class autoloader
 	* @see PtcHandyMan::addFiles()
 	* @param	array	$directories	the full path to the directories holding the classes
 	*/
@@ -186,24 +146,57 @@
 		$handyman = '_PTCHANDYMAN_' . @strtoupper( @str_replace( '\\' , '_' , __NAMESPACE__ ) ) . '_';
 		return ptc_run( 'addFiles' , @constant( $handyman ) , array( $files ) );
 	}
+	/**
+	* Gets protected and private properties
+	* @see PtcHandyMan::getProperty()
+	* @param	mixed	$class			the name or the initialized class object	
+	* @param	string	$propertyName	the name of the property
+	*/
+	function ptc_get_prop( $class , $propertyName )
+	{
+		$handyman = '_PTCHANDYMAN_' . @strtoupper( @str_replace( '\\' , '_' , __NAMESPACE__ ) ) . '_';
+		return ptc_run( 'getProperty' , @constant( $handyman ) , array( $class , $propertyName ) );
+	}
+		
+	/** EVENT DISPATCHER COMPONENT HELPERS **************************************************/
 	
+	/**
+	* Adds a listener to an event
+	* @see PtcEvent::listen()
+	* @param	string	$event		the event name, example: "event.sub_event"
+	* @param	mixed	$callback		a valid callback ( closure , function , class , class@method )
+	* @param	numeric	$priority		a numeric value, higher values will execute first
+	*/
+	function ptc_listen( $event , $callback , $priority = 0 )
+	{
+		$ptc_event = '_PTCEVENT_' . @strtoupper( @str_replace( '\\' , '_' , __NAMESPACE__ ) ) . '_';
+		return ptc_run( 'listen' , @constant( $ptc_event ) , array( $event , $callback , $priority ) );
+	}
+	/**
+	* Fires an event
+	* @see PtcEvent::fire()
+	* @param	string	$event	the event name to fire
+	* @param	array	$data	an array with the data you wish to pass to the listeners
+	*/
+	function ptc_fire( $event , $data )
+	{
+		$ptc_event = '_PTCEVENT_' . @strtoupper( @str_replace( '\\' , '_' , __NAMESPACE__ ) ) . '_';
+		return ptc_run( 'fire' , @constant( $ptc_event ) , array( $event , $data ) );
+	}
 	
 	/**** PHPTOOLCASE RUNNER UTILITY ********************************************************/
 	
 	/**
 	* Runs class methods 
+	* @param	string	$function		the function/method to run
 	* @param	string	$class		the class name with it's full namespace
-	* @param	string	$function		the method to run
 	* @param	array	$args		an array with arguments for the method
-	* @return	returns  the result of the call , or false if the method is not callable
+	* @return	returns the result of the call, or false if the method was not callable
 	*/
 	function ptc_run( $function , $class = null , $args = array( ) )
 	{
 		$call = ( $class ) ? array( '\\' . $class , $function ) : $function;
-		if ( @is_callable( $call ) )
-		{		
-			return call_user_func_array( $call ,  $args  );
-		}
+		if ( @is_callable( $call ) ) { return call_user_func_array( $call , $args ); }
 		//trigger_error( 'Could not run method "' . $function .'" for class "' . $class . '"' , E_USER_ERROR );
 		return false;
 	}
