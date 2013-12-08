11460af789ce2609a959fa2b30cdfa96e11ea686
Carlo Pietrobattista <carlo@salapc.com>
2013-12-08 23:44:24 +0100

    autoloader component

--- PtcHm.php
+++ PtcHm.php
@@ -3,9 +3,8 @@
 	/**
 	* PHP TOOLCASE HANDYMAN CLASS
 	* PHP version 5.3
-	* @category 	Libraries
-	* @package  	PhpToolCase
-	* @version	0.9
+	* @category 	Library
+	* @version	0.9.2
 	* @author   	Irony <carlo@salapc.com>
 	* @license  	http://www.gnu.org/copyleft/gpl.html GNU General Public License
 	* @link     	http://phptoolcase.com
@@ -13,48 +12,37 @@
 
 	class PtcHandyMan			
 	{
-		public function __construct( $options=null , $debugOptions=null )
-		{		
-			/* load the debugger & logger if present */
-			/*if($debugOptions && !defined('PTCDEBUG_LOADED'))
-			{
-				if(class_exists(__NAMESPACE__.'\PtcDebug'))
-				{
-					PtcDebug::load($debugOptions); 
-				}
-				else if($debugOptions && class_exists('\PtcDebug'))
-				{
-					\PtcDebug::load($debugOptions); 
-				}
-			}*/
-		}
 		/**
-		* Alias of {@link addDirs()}
+		* Alias of @ref addDirs()
 		*/
 		public static function addDir( $path ) { static::addDirs( $path ); }
 		/**
-		* Alias of {@link addFiles()}
+		* Alias of @ref addFiles()
 		*/
 		public static function addFile( $file ){ static::addFiles( $file ); }
 		/**
-		* Alias of {@link addSeparator()}
+		* Alias of @ref addSeparator()
 		*/
 		public static function addSeparator( $sep ) { static::addSeparators( $sep ); }
 		/**
-		* Alias of {@link addConventions()}
+		* Alias of @ref addConventions()
 		*/
-		public static function addConvention( $conventions ) { static::addConventions( $conventions ); }
+		public static function addConvention( $conventions ) 
+		{ 
+			static::addConventions( $conventions ); 
+		}
 		/**
-		* Alias of {@link addAppPaths()}
+		* Alias of @ref addAppPaths()
 		*/
 		public static function addAppPath( $path ) { static::addAppPaths( $path ); }
 		/**
-		* Alias of {@link getAppPaths()}
+		* Alias of @ref getAppPaths()
 		*/
 		public static function getAppPath( $type = null ) { return static::getAppPaths( $type ); }
 		/**
-		* Adds path(s) to the {@link $_directory} property to autoload classes when needed 
-		* @param	string|array 	$paths	full path/s to directory with classes
+		* Adds path(s) to the PtcHandyMan::$_dirs property to load classes when needed. 
+		* See @ref add_dirs and @ref namespace_dirs
+		* @param	string|array		$paths	full path(s) to directories with classes
 		*/
 		public static function addDirs( $paths )
 		{
@@ -65,19 +53,23 @@
 				if ( $path )				// if path exists
 				{	
 					$result = ( !is_int( $k ) ) ? static::_addNamespaceDirectory( 
-								str_replace( '\\' , '' , $k ) , $path ) : static::_addDirectory( $path );
+						str_replace( '\\' , '' , $k ) , $path ) : static::_addDirectory( $path );
 					if ( !$result ) { unset( $paths[ $k ] ); continue; }
 				}
 				else						// need to trigger error if path is not found
 				{
-					trigger_error( 'The path "' . $v . '" does not exists or is not accessible!' , E_USER_ERROR );
+					trigger_error( 'The path "' . $v . 
+								'" does not exists or is not accessible!' , E_USER_ERROR );
 				}
 			}
-			if ( $paths ) { static::_debug( $paths , 'Added path(s) to autoloader' , 'Autoloader' ); } // debug
+			if ( $paths ) // debug
+			{ 
+				static::_debug( $paths , 'Added path(s) to autoloader' , 'Autoloader' ); 
+			} 
 		}
 		/**
-		* Adds a class file to the autoloader
-		* @param	string	$files		full path of the class file		
+		* Adds class file(s) to the autoloader. See @ref add_files
+		* @param	string		$files		full path of the class file		
 		*/
 		public static function addFiles( $files )
 		{
@@ -87,8 +79,10 @@
 				$file = realpath( $v );		// get real OS path
 				if( $file )					// path exists
 				{	
-					$key = ( substr( $k , 0 , 1 ) == '\\' ) ? substr( $k , 1 ) : $k; // remove first '\'  if present
-					if ( @array_key_exists( $key , @static::$_dirs[ 'files' ] ) ) // check if class  name exists  already
+					// remove first '\'  if present
+					$key = ( substr( $k , 0 , 1 ) == '\\' ) ? substr( $k , 1 ) : $k; 
+					// check if class  name exists  already
+					if ( @array_key_exists( $key , @static::$_dirs[ 'files' ] ) ) 
 					{
 						trigger_error( 'Cannot redeclare "' . $k . '" as class name!' , E_USER_ERROR );
 						unset( $files[ $k ] );
@@ -98,38 +92,43 @@
 				}
 				else { trigger_error( 'the file "' . $v . '" cannot be found!' , E_USER_ERROR ); } // file not found
 			}
-			if ( $files )	{ static::_debug( $files , 'Added file(s) to autoloader' , 'Autoloader' ); } 	// debug
+			if ( $files )	{ static::_debug( $files , 'Added file(s) to autoloader' , 'Autoloader' ); } // debug
 		}
 		/** 
-		* Registers the autoloader to load classes when needed
+		* Registers the autoloader to load classes when needed. See @ref hm_getting_started
 		* @param	bool		$addThisPath			adds the path where the class resides as a directory
-		* @param	bool		$useHelpers			load the ptc-helpers.php file if in this directory
-		* @param	bool		$registerAutoLoader	regiisters the load method with te spl utilities
+		* @param	bool		$useHelpers			load the ptc-helpers.php file if found, and registers the event class
+		* @param	bool		$registerAutoLoader		registers the load method with the spl utilities
 		*/
 		public static function register( $addThisPath = true , $useHelpers = true , $registerAutoLoader = true )
 		{
 			$this_class = get_called_class( );
 			if ( $addThisPath ) { static::addDir( dirname( __FILE__ ) ); }	// add this path
-			if ( $useHelpers && file_exists( dirname( __FILE__ ) . '/ptc-helpers.php' ) ) // add the helpers if found
+			if ( $registerAutoLoader ) { spl_autoload_register( array( $this_class , 'load' ) ); }
+			if ( $useHelpers && file_exists( dirname( __FILE__ ) . '/ptc-helpers.php' ) ) // add helpers if found
 			{ 
 				require_once( dirname( __FILE__ ) . '/ptc-helpers.php' ); 
 			}
-			if ( $registerAutoLoader ) { spl_autoload_register( array( $this_class , 'load' ) ); }
+			if ( $useHelpers && file_exists( dirname( __FILE__ ) . '/PtcEvent.php' ) ) 
+			{ 
+				__NAMESPACE__ . PtcEvent::register( ); // register PtcEvent with helpers
+			}
 			$namespace = @strtoupper( @str_replace( '\\' , '_' , __NAMESPACE__ ) ) . '_';
 			@define( '_PTCHANDYMAN_' . $namespace , $this_class ); 		// declare the class namespace
 			static::_debug( 'Autoloader registerd' , '' , 'Autoloader' );
 		}
 		/**
-		* Gets the separators used by the autoloader
+		* Retrieves the separators used by the autoloader
 		*/
 		public static function getSeparators( ){ return static::$_separators; }
 		/**
-		* Gets the naming conventions used by the autoloader
+		* Retrieves the naming conventions used by the autoloader
 		*/		
 		public static function getConventions( ){ return static::$_namingConventions; }
 		/**
-		* Adds a separator to the {@link $_separators} property for the autoloader
-		* @param	array|string	$sep		the separator(s) to be added
+		* Adds a separator to the  PtcHandyMan::$_separators property for the autoloader. 
+		* See @ref using_separators
+		* @param	array|string		$sep		the separator(s) to be added
 		*/
 		public static function addSeparators( $sep )
 		{
@@ -145,8 +144,9 @@
 			}
 		}
 		/**
-		* Adds naming convention(s) to the {@link $_namingConventions} property for the autoloader
-		* @param	array|string	$sep		the separator(s) to be added
+		* Adds naming convention(s) to the PtcHandyMan::$_namingConventions property for the autoloader.
+		* See @ref using_separators
+		* @param	array|string		$conventions		the naming convention(s) to be added
 		*/
 		public static function addConventions( $conventions )
 		{
@@ -162,16 +162,16 @@
 			}
 		}
 		/**
-		* Returns the current included paths for  the autoloader
-		* @param	string	$type	( directories , ns , files )
-		* @return			returns the $_dirs property based on the $type parameter
+		* Returns the current included paths for  the autoloader. See @ref getDirs
+		* @param	string		$type		directories , ns , files
+		* @return	the PtcHandyMan::$_dirs property based on the $type argument
 		*/
 		public static function getDirs( $type = null )
 		{ 
 			if ( $type && !in_array( $type , array( 'directories' , 'ns' , 'files' ) ) ) // wrong parameter
 			{ 
-				trigger_error( 'No directories are present with the "' . $type . '" parameter!' , 
-																E_USER_WARNING );
+				trigger_error( 'No directories are present with the "' . 
+								$type . '" parameter!' , E_USER_WARNING );
 				return;
 			}
 			else if ( $type && !@static::$_dirs[ $type ] ) { return false; } // no values in array
@@ -179,22 +179,23 @@
 			return @static::$_dirs;								// return all values
 		}
 		/**
-		* Helper method to retrieve paths for the application
-		* @param	string	$type	the path to return stored in the $_appPaths property
+		* Helper method to retrieve paths for the application. See @ref usingAddedPath
+		* @param	string		$name		the path to return stored in the PtcHandyMan::$_appPaths property
+		* @return	the PtcHandyMan::$_appPaths property based on the $name argument
 		*/
-		public static function getAppPaths( $type = null )
+		public static function getAppPaths( $name = null )
 		{
 			if ( empty( static::$_appPaths ) ) { static::_buildAppPaths( ); } // build paths once
-			if ( !$type ) { return static::$_appPaths; } // return all application paths by default
-			if ( !@array_key_exists( $type , static::$_appPaths ) )
+			if ( !$name ) { return static::$_appPaths; } // return all application paths by default
+			if ( !@array_key_exists( $name , static::$_appPaths ) )
 			{
-				trigger_error( 'No paths found with the option "' . $type . '"!' , E_USER_WARNING );
+				trigger_error( 'No paths found with the option "' . $name . '"!' , E_USER_WARNING );
 				return;
 			}
-			return static::$_appPaths[ $type ];
+			return static::$_appPaths[ $name ];
 		}
 		/**
-		* Adds paths to the the $_appPath property for the helper method getAppPath 
+		* Adds paths to the PtcHandyMan::$_appPaths property for later usage. See @ref addingAppPath 
 		* @param	array 		$paths		array of paths to add
 		*/
 		public static function addAppPaths( $paths )
@@ -204,8 +205,8 @@
 			{
 				if ( !$path = @realpath( $v ) )
 				{
-					trigger_error( 'The file or path "' . $v . '" does not exists or is not accessible!' , 
-																	E_USER_ERROR ); 
+					trigger_error( 'The file or path "' . $v .
+								'" does not exists or is not accessible!' , E_USER_ERROR ); 
 					return;
 				}
 				@$add_paths[ $k ] = $path; 
@@ -213,9 +214,39 @@
 			static::$_appPaths = array_merge( static::$_appPaths , $add_paths );
 		}
 		/**
-		* Load classes automatically with namespaces support based on folder structure
-		* @param	string 	$class	the name of the class to autoload
-		* @return					returns true if a file has been loaded, false otherwise
+		* Retrieves inaccessible properties from a class or object. See @ref read_properties
+		* @param	mixed		$object		the name of the class or the initialized object
+		* @param	string		$propertyName	the name of the property
+		* @return	the value of the property if found.
+		*/
+		public static function getProperty( $object , $propertyName )
+		{
+			if ( !$object ){ return null; }
+			if ( is_string( $object ) )	// static property
+			{
+				if ( !class_exists( $object ) ){ return null; }
+				$reflection = new \ReflectionProperty( $object , $propertyName );
+				if ( !$reflection ){ return null; }
+				$reflection->setAccessible( true );
+				return $reflection->getValue( );
+			}
+			$class = new \ReflectionClass( $object );
+			if ( !$class ){ return null; }
+			if( !$class->hasProperty( $propertyName ) ) // check if property exists
+			{
+				trigger_error( 'Property "' . $propertyName . '" not found in class "' . 
+									get_class( $object ) . '"!' , E_USER_WARNING );
+				return null;
+			}
+			$property = $class->getProperty( $propertyName );
+			$property->setAccessible( true );
+			return $property->getValue( $object );
+		}		
+		/**
+		* Load classes automatically with namespaces support based on folder structure. 
+		* See @ref adding_dirs
+		* @param	string 	$class		the name of the class to autoload
+		* @return	true if a file has been loaded, false otherwise.
 		*/
 		public static function load( $class )
 		{
@@ -242,8 +273,8 @@
 					}
 					if ( !@realpath( $path ) )
 					{
-						trigger_error( 'The path "' . $path . '" does not exists or is not accessible!' , 
-																		E_USER_ERROR ); 
+						trigger_error( 'The path "' . $path . 
+									'" does not exists or is not accessible!' , E_USER_ERROR ); 
 						return false;
 					}
 					if ( $found = static::_loadClass( $class_name , $path , $class ) ) { return true; }
@@ -259,18 +290,15 @@
 			return false;
 		}
 		/**
-		* Paths for diretories to autoload classes
-		* @var 	array 
+		* Paths for directories to autoload classes
 		*/
-		protected static $_dirs=array();
+		protected static $_dirs = array();
 		/**
 		* Separators for naming conventions
-		* @var 	array 
 		*/
 		protected static $_separators = array( );
-		/*
-		* Naing Convetions to attempt to load with the autoloader
-		* @var 	array 
+		/**
+		* Naming conventions to attempt to load with the autoloader
 		*/		
 		protected static $_namingConventions = array( );
 		/**
@@ -279,10 +307,10 @@
 		protected static $_appPaths = array( );
 		/**
 		* Classes autoloader engine, tries various possibilities for file names
-		* @param	string	$class		the class name without namespaces
-		* @param	string	$path		the full path to the file
-		* @param	string	$namespace	the class name with namespace if present
-		* @return						returns true if a file is found, false otherwise
+		* @param	string		$class		the class name without namespaces
+		* @param	string		$path		the full path to the file
+		* @param	string		$namespace	the class name with namespace if present
+		* @return	true if a file is found, false otherwise
 		*/
 		protected static function _loadClass( $class , $path , $namespace = null )
 		{
@@ -302,14 +330,15 @@
 				require_once(  $path . $new_file );
 				return true;
 			}
-			else { return static::_guessFileName( $class , $path , $namespace ); } // use an engine to guess the file
+			else{ return static::_guessFileName( $class , $path , $namespace ); } // use an engine to guess the file
 			return false;
 		}
 		/**
-		* Tries to guess the filename to load based on the naming convetions and the separator properties
-		* @param	$class			the class name without namespaces
-		* @param	$className		the class name with namespaces
-		* @param	$path
+		* Tries to guess the filename to load based on the naming conventions and the separator properties
+		* @param	string		$class		the class name without namespaces
+		* @param	string		$path		the full path to the file
+		* @param	string		$namespace	the class name with namespace if present
+		* @return	true if a file is found, false otherwise
 		*/
 		protected static function _guessFileName( $class , $path , $namespace = null )
 		{
@@ -318,12 +347,12 @@
 			{
 				foreach ( static::$_namingConventions as $convention )
 				{
-					$filename = str_replace( array( '{SEP}' , '{CLASS}' ) , array( $sep , $class ) , 
-																		$convention );
+					$filename = str_replace( array( '{SEP}' , '{CLASS}' ) , 
+										array( $sep , $class ) , $convention );
 					if ( file_exists( $path . $filename  . '.php') ) // try new filename
 					{
-						static::_debug( array( 'file' => $path . $filename . '.php' , 'class' => $class_name ) , 
-												'Included class file' , 'Autoloader' ); // debug
+						static::_debug( array( 'file' => $path . $filename . '.php' , 
+								'class' => $class_name ) , 'Included class file' , 'Autoloader' ); // debug
 						require_once(  $path . $filename .'.php');
 						return true;
 					}
@@ -339,15 +368,15 @@
 						$replaced = str_replace( '_' , $sep , $filename ); // try new file name
 						if ( file_exists( $path . $replaced . '.php' ) )
 						{
-							static::_debug( array( 'file' =>$path . $replaced . '.php' , 'class' => $class_name ) , 
-															'Included class file','Autoloader');	// debug
+							static::_debug( array( 'file' =>$path . $replaced . '.php' , 
+									'class' => $class_name ) , 'Included class file','Autoloader');	// debug
 							require_once( $path . $replaced . '.php'); 
 							return true; 
 						}
 						else if ( file_exists( $file = $path . strtolower( $replaced ) . '.php' ) )	// lowercase
 						{
-							static::_debug( array( 'file' => $file , 'class' => $class_name ) , 
-												'Included class file' , 'Autoloader' ); // debug
+							static::_debug( array( 'file' => $file , 
+								'class' => $class_name ) , 'Included class file' , 'Autoloader' ); // debug
 							require_once( $file ); 
 							return true; 
 						}
@@ -357,24 +386,24 @@
 			return false;
 		}
 		/**
-		* Build application paths for the first time when getAppPath( ) is called
+		* Builds application paths for the first time when PtcHandyMan::getAppPath( ) is called.
 		*/
 		protected static function _buildAppPaths( )
 		{
 			static::$_appPaths = array
 			(
-				'docRoot' 		=> 	@$_SERVER[ 'DOCUMENT_ROOT' ] , // the document root folder
+				'docRoot' 	=> 	@$_SERVER[ 'DOCUMENT_ROOT' ] , // the document root folder
 				'handyMan'	=>	dirname( __FILE__ ) // the directory where this file resides
 			);
 		}
 		/**
-		* Adds a directory to the {@link $_dirs} array to autoload classes
-		* @param	string	$directory		the full path to the directory
-		* @return			returns false if directory is already present, true otherwise
+		* Adds a directory to the PtcHandyMan::$_dirs array to autoload classes
+		* @param	string		$directory		the full path to the directory
+		* @return	false if directory is already present, true otherwise
 		*/
 		protected static function _addDirectory( $directory )
 		{
-			if ( @in_array( $directory , @static::$_dirs[ 'directories' ] ) )	// check if directory is already present
+			if ( @in_array( $directory , @static::$_dirs[ 'directories' ] ) ) // check if dir is already present
 			{
 					static::_debug( $directory , 'Path already exists!' , 'Autoloader' );	// debug
 					return false;
@@ -383,26 +412,26 @@
 			return true;
 		}
 		/**
-		* Adds a directory to the {@link $_dirs} array to autoload classes that are namespaced
-		* @param	string	$namespace	the namespace for the path toa utoload classes
-		* @param	string	$directory		the full path to the directory
-		* @return	returns true if directory is not present, triggers an error otherwise 
+		* Adds a directory to the PtcHandyMan::$_dirs array to load classes that use namespaces
+		* @param	string		$namespace		the namespace for the path toa utoload classes
+		* @param	string		$directory		the full path to the directory
+		* @return	true if directory is not present, triggers an error otherwise 
 		*/		
 		protected static function _addNamespaceDirectory( $namespace , $directory )
 		{
-			if ( @array_key_exists( $namespace , @static::$_dirs[ 'ns' ] ) ) // check if directory is already present
+			if ( @array_key_exists( $namespace , @static::$_dirs[ 'ns' ] ) ) // check if dir is already present
 			{
-					trigger_error( 'Cannot redeclare namespace "' . $namespace . '"!' , E_USER_ERROR );
-					return false;
+				trigger_error( 'Cannot redeclare namespace "' . $namespace . '"!' , E_USER_ERROR );
+				return false;
 			}
 			@static::$_dirs[ 'ns' ][ $namespace ] = $directory;
 			return true;
 		}
 		/**
 		* Send messsages to the PtcDebug class if present
-		* @param 	mixed 	$string		the string to pass
-		* @param 	mixed 	$statement	some statement if required
-		* @param	string	$category	a category for the messages panel
+		* @param 	mixed 		$string		the string to pass
+		* @param 	mixed 		$statement		some statement if required
+		* @param	string		$category		a category for the messages panel
 		*/
 		protected static function _debug( $string , $statement = null , $category = null )
 		{
