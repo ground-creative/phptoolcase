9798a90fcfd98e55df4d4de0f72e2e2ab8ba800d
Daniele Viti <dnviti@gmail.com>
2019-04-07 12:44:17 +0200

    Problem on inserting/updating/selecting null values
    Hi, the method that parse the value to be selected/updated/inserted has a problem: if i try to insert a value of null it parses it to 0, found the problem, here's the solution:
    
    the method "_bind" should change from this:
    
    ```php
      /**
         * Binds values to the query
         * @param	mixed		$pos		the param position if numeric
         * @param	mixed		$value		the value to bind the place holder to
         * @param	contants	$type		a pdo constant to bind values
         */
        protected function _bind($pos, $value, $type = null)
        {
            if (is_numeric($pos)) {
                $pos = ($pos + 1);
            }
            if (is_null($type)) {
                switch ($value) {
                    case is_int($value):
                        $type = \PDO::PARAM_INT;
                        break;
                    case is_bool($value):
                        $type = \PDO::PARAM_BOOL;
                        break;
                    case is_null($value):
                        $type = \PDO::PARAM_NULL;
                        break;
                    default:
                        $type = \PDO::PARAM_STR;
                }
            }
            $this->_query->bindValue($pos, $value, $type);
        }
    ```
    to this:
    
    ```php
        /**
         * Binds values to the query
         * @param	mixed		$pos		the param position if numeric
         * @param	mixed		$value		the value to bind the place holder to
         * @param	contants	$type		a pdo constant to bind values
         */
        protected function _bind($pos, $value, $type = null)
        {
            if (is_numeric($pos)) {
                $pos = ($pos + 1);
            }
            if (is_null($type)) {
                switch ($value) {
                    case is_null($value):
                        $type = \PDO::PARAM_NULL;
                        break;
                    case is_bool($value):
                        $type = \PDO::PARAM_BOOL;
                        break;
                    case is_int($value):
                        $type = \PDO::PARAM_INT;
                        break;
                    default:
                        $type = \PDO::PARAM_STR;
                }
            }
            $this->_query->bindValue($pos, $value, $type);
        }
    ```
    
    that means that the parsing of null values should be done on top of the switch statement, otherwise the is_int() function will interpret the "" value of an input field to (int)0 and that is not correct because NULL is not 0

--- PtcQueryBuilder.php
+++ PtcQueryBuilder.php
@@ -635,11 +635,11 @@
 			{
 				switch ( $value ) 
 				{
+					case is_null( $value ): $type = \PDO::PARAM_NULL;
+					break;
 					case is_int( $value ): $type = \PDO::PARAM_INT;
 					break;
 					case is_bool( $value ): $type = \PDO::PARAM_BOOL;
-					break;
-					case is_null( $value ): $type = \PDO::PARAM_NULL;
 					break;
 					default: $type = \PDO::PARAM_STR;
 				}
