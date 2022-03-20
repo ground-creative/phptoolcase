57f5619a90f5be0a3fe80941d7c4506e0e7c25c5
ground-creative <irony00100@gmail.com>
2022-03-20 10:40:48 +0000

    ci: update version to 1.1.7

--- PtcEvent.php
+++ PtcEvent.php
@@ -5,7 +5,7 @@
 	* PHP version 5.4+
 	* @category 	Libraries
 	* @package  	PhpToolCase
-	* @version	1.9.4
+	* @version	1.1.7
 	* @author   	Irony <carlo@salapc.com>
 	* @license  	http://www.gnu.org/copyleft/gpl.html GNU General Public License
 	* @link     	http://phptoolcase.com
--- ptc-helpers.php
+++ ptc-helpers.php
@@ -4,7 +4,7 @@
 	* PHPTOOLCASE HELPER FUNCTIONS FILE
 	* PHP version 5.4+
 	* @category 	Library
-	* @version	1.9.4
+	* @version	1.1.7
 	* @author   	Irony <carlo@salapc.com>
 	* @license  	http://www.gnu.org/copyleft/gpl.html GNU General Public License
 	* @link     	http://phptoolcase.com
