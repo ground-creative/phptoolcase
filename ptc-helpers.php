7719b882b45c6d382903cf7cb749f2387da787c3
ifsale <irony00100@gmail.com>
2022-03-20 04:08:49 +0000

    ci: update version to v1.2.6

--- ptc-helpers.php
+++ ptc-helpers.php
@@ -4,7 +4,7 @@
 	* PHPTOOLCASE HELPER FUNCTIONS FILE
 	* PHP version 5.4+
 	* @category 	Library
-	* @version	1.2.4
+	* @version	1.2.6
 	* @author   	Irony <carlo@salapc.com>
 	* @license  	http://www.gnu.org/copyleft/gpl.html GNU General Public License
 	* @link     	http://phptoolcase.com
