5c287e9cb9bff1437f78bc88bc080d2b0b59c1e9
ground-creative <irony00100@gmail.com>
2022-03-21 18:02:16 +0000

    ci: update version to v1.0.1-stable

--- PtcView.php
+++ PtcView.php
@@ -1,10 +1,10 @@
 <?php
 
-	/** A
+	/**
 	* PHPTOOLCASE VIEW CLASS
 	* PHP version 5.4+
 	* @category 	Library
-	* @version	v1.0.1-stable
+	* @version	v1.0.0-stable
 	* @author   	Irony <carlo@salapc.com>
 	* @license  	http://www.gnu.org/copyleft/gpl.html GNU General Public License
 	* @link     	http://phptoolcase.com
