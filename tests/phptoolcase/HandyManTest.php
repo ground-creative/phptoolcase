95fdec6a9a28fba351e4c328bfa00856cc6fa32f
Carlo <carlo@ground-creative.com>
2022-05-27 00:55:15 +0700

    added more unit tests

--- tests/phptoolcase/HandyManTest.php
+++ tests/phptoolcase/HandyManTest.php
@@ -6,5 +6,93 @@
 
 	final class HandyManTest extends TestCase
 	{
-	
+		public function testAddDirectory( )
+		{
+		
+		}
+		
+		public function testAddFile( )
+		{
+		
+		}
+		
+		public function testAddSeparator( )
+		{
+		
+		}
+		
+		public function testAddConvention( )
+		{
+		
+		}
+		
+		public function testAddAppPath( )
+		{
+		
+		}
+		
+		public function testGetAppPath( )
+		{
+		
+		}
+		
+		public function testArraySetValue( )
+		{
+		
+		}
+		
+		public function testArrayGetValue( )
+		{
+		
+		}
+		
+		public function testArrayCount( )
+		{
+		
+		}
+		
+		public function testArrayDel( )
+		{
+		
+		}
+		
+		public function testAddAlias( )
+		{
+		
+		}
+		
+		public function testGetAlias( )
+		{
+		
+		}
+		
+		public function testSessionSetVariable( )
+		{
+		
+		}
+		
+		public function testSessionGetVariable( )
+		{
+		
+		}
+		
+		public function testSessionDeleteVariable( )
+		{
+		
+		}
+		
+		public function testSessionTypes( )
+		{
+		
+		}
+		
+		public function testJsonResponse( )
+		{
+		
+		}
+		
+		public function testGetInaccessibleObjectProperty( )
+		{
+		
+		}
 	}
